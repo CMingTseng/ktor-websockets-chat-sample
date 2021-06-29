6c2812b235a34dc5479b012204e0a65734cc8ba9
Neo <c.min.tseng@gmail.com>
2021-06-30 06:18:51 +0800

    modify rootProject build.gradle.kts & export some session into subProject build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,27 @@
+buildscript {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+        jcenter()
+        google()
+    }
+
+    dependencies {
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.5.10"
 }
 
-group = "com.jetbrains.handson"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    jcenter()
-    mavenCentral()
-}
-
-val ktor_version: String by project
-val logback_version: String by project
-
-dependencies {
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("io.ktor:ktor-websockets:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+subprojects{
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        jcenter()
+        gradlePluginPortal()
+        google()
+    }
 }
