d6cc8452a958bcba089291d68794abf1d3ec81db
Neo <c.min.tseng@gmail.com>
2021-06-30 14:00:53 +0800

    add lost server side class

--- client/gui_mode/build.gradle.kts
+++ client/gui_mode/build.gradle.kts
@@ -6,8 +6,9 @@ group = "com.jetbrains.handson"
 version = "1.0-SNAPSHOT"
 
 val ktor_version: String by project
-
+val logback_version: String by project
 dependencies {
     implementation("io.ktor:ktor-client-websockets:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
 }
